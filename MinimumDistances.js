76d674a0117b8244a760fff00144bbfbd6a3ab5e
Pavanar10 <pavanar10@gmail.com>
2024-07-09 15:42:06 +0300

    Added minimum distance problem

--- MinimumDistances.js
+++ MinimumDistances.js
@@ -8,6 +8,25 @@ function minimumDistances(a){
         return acc;
       }, []);
       
-      console.log(Math.min(...repeatingIndexes));
-}
+      return (Math.min(...repeatingIndexes));
+      
+} 
+//another solution
+/* function minimumDistances(a){
+    let res=[];
+    for(let i=0;i<a.length;i++ ){
+        let lastIndex=a.lastIndexOf(a[i]);
+          if(lastIndex!==-1 && (lastIndex-i)>0){
+           res.push(lastIndex-i)
+           continue
+          }
+    }
+    console.log(res)
+if(res.length>0){
+    return Math.min(...res)
+    }
+    else{
+        return -1
+    }
+} */
 console.log(minimumDistances([7,1,3,4,1,7]));
